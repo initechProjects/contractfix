84738aeb31634f6970645e1ee3c0025be8dc7f70
Ahmet Cetin <ahmet@cetin.info>
2015-12-16 00:09:33 +0100

    Contract save, open

--- server/contract/controller/contract.js
+++ server/contract/controller/contract.js
@@ -5,34 +5,14 @@ var Boom   = require('boom');
 var Contract   = require('../model/contract').Contract;
 var _ = require('lodash');
 
-exports.create = {
-  auth: {
-    strategy: 'token',
-    scope: ['registered']
-  },
+exports.save = {
   validate: {
     payload: {
       contractId: Joi.string(),
-      original: Joi.string().required()
-    }
-  },
-  handler: function(request, reply) {
-
-    request.payload.users = [request.auth.credentials._id];
-    Contract.saveContract(request.payload, function(err, contract) {
-      if (!err) {
-        reply('Contract has been created');
-      } else {
-        reply(Boom.forbidden(err)); // HTTP 403
-      }
-    });
-  }
-};
-
-exports.update = {
-  validate: {
-    payload: {
-      contractId: Joi.string().required()
+      templateId: Joi.string(), //Joi.when('contractId', { is: undefined, then: Joi.string().required() }),
+      text: Joi.string().required(),
+      comments: Joi.array(),
+      personal: Joi.boolean()
     }
   },
   auth: {
@@ -40,17 +20,80 @@ exports.update = {
   },
   handler: function(request, reply) {
     if (request.auth.isAuthenticated) {
-      Contract.findContract(request.payload.contractId, function(err, contract) {
-        if (!err) {
-          // if user belongs to any group required by the contract
-          if (_.intersection(contract.users, request.auth.credentials.scope).length > 0) {
-            // Here update code
+      if (request.payload.contractId) {
 
+        Contract.findContract(request.payload.contractId, function(err, contract) {
+          if (!err) {
+            if (!contract) { reply(Boom.badImplementation('wrong contractId')); }
+
+            // if user belongs to any group required by the contract
+            if (_.intersection(contract.users, request.auth.credentials.scope).length > 0) {
+              // Here update code
+              let version = {
+                userid: request.auth.credentials._id,
+                versionDate: Date.now(),
+                text: request.payload.text
+              };
+
+              if (request.payload.personal) {
+                contract.drafts.push(version);
+              } else {
+                contract.versions.push(version);
+              }
+
+              request.payload.comments.forEach(function(item) {
+                let comment = {
+                  userid: request.auth.credentials._id,
+                  commentDate: Date.now(),
+                  text: item
+                };
+
+                if (!contract.comments) contract.comments = [];
+                contract.comments.push(comment);
+              });
+
+              Contract.updateContract(contract, function(err) {
+                if (!err) {
+                  reply('Contract has been updated');
+                } else {
+                  reply(Boom.badImplementation(err));
+                }
+              });
+
+            } else {
+              reply(Boom.forbidden("You don't have privileges for this contract."));
+            }
+          } else {
+            reply(Boom.badImplementation(err));
           }
-        }
-      });
+        });
 
-      reply('Contract has been updated');
+      } else {
+        // Create new contract
+        let contract = {};
+
+        contract.metadata = {};
+        contract.metadata.dateCreated = Date.now();
+        if (request.payload.templateId) contract.metadata.templateId = request.payload.templateId;
+
+        contract.versions = [];
+        let version = {
+          userid: request.auth.credentials._id,
+          versionDate: Date.now(),
+          text: request.payload.text
+        };
+        contract.versions.push(version);
+
+        contract.users =[request.auth.credentials._id];
+
+        Contract.newContract(contract, function(err) {
+          if (!err) {
+            reply('Contract has been saved');
+          } else {
+            reply(Boom.badImplementation(err));
+          }
+        });
+      }
     }
   }
 };
@@ -70,6 +113,57 @@ exports.findContractByUserId = {
         });
         reply(contractsList);
       });
+    }
+  }
+};
+
+exports.open = {
+  auth: {
+    strategy: 'token'
+  },
+  handler: function(request, reply) {
+    if (request.auth.isAuthenticated) {
+
+      let contractId = request.payload.contractid;
+      if (!contractId) reply(Boom.badImplementation('No contract id'));
+
+      Contract.findContract(contractId, function(err, contract) {
+        if (err) reply(Boom.badImplementation(err));
+        if (!contract) reply(Boom.badImplementation('wrong contractId'));
+        if (_.intersection(contract.users, request.auth.credentials.scope).length === 0) reply(Boom.forbidden("You don't have privileges for this contract."));
+
+        let result = {};
+
+        result.contractId = contract._id;
+        result.metadata = contract.metadata;
+        result.comments = contract.comments;
+
+        if (contract.drafts) {
+          let l = contract.drafts.length;
+
+          while (l--) {
+            if (contract.drafts[l].userid === request.auth.credentials._id) {
+              result.personal = {};
+              result.personal.text = contract.drafts[l].text;
+              result.personal.versionDate = contract.drafts[l].versionDate;
+              break;
+            }
+          }
+        }
+
+        result.revisions = contract.versions.length;
+
+        // console.log(result);
+        result.latest = {};
+        result.latest.text = contract.versions[result.revisions - 1].text;
+        result.latest.versionDate = contract.versions[result.revisions - 1].versionDate;
+        result.previous = {};
+        result.previous.text = contract.versions[result.revisions - 2].text;
+        result.previous.versionDate = contract.versions[result.revisions - 2].versionDate;
+
+        reply(result);
+      });
+
     }
   }
 };
