a6269128c9bc12c7abe04f1d6353776417220db0
Hera Kim <herashk@gmail.com>
2015-12-22 01:21:01 -0500

    started fixing user invitation

--- client/src/app/auth/invitation.js
+++ client/src/app/auth/invitation.js
@@ -1,14 +1,11 @@
 /*-----------------------------------------------------------------------------
-*
 * This angular module encompasses all controllers and services
 * related to the redirection and processing of invited users.
-*
 * If a user does not have an account, they will be asked to provide
 * a full name before being registered.  If the invited user already
 * has an account, they will be redirected to the login page. In
 * either case, the user will arrive at the editor page after
 * registration/login.
-*
 -----------------------------------------------------------------------------*/
 
 angular.module('app.invitation', [])
@@ -19,7 +16,7 @@ angular.module('app.invitation', [])
   var url = $location.url();
   var token = url.slice(url.indexOf("=")).slice(1);
 
-  $scope.validateInvite = function(){
+  $scope.invitation = function(){
 
     $http({
       method: 'POST',
@@ -31,10 +28,7 @@ angular.module('app.invitation', [])
     })
     .success(function(data){
       $scope.response = data;
-      $window.localStorage.username = data.username;
-      $window.localStorage.contractid = data.contractid;
-
-      $window.location.href =  data.newuser === true  ?  '/#/signup2' : '/#/login';
+    
 
     })
     .catch(function(err){
@@ -42,10 +36,4 @@ angular.module('app.invitation', [])
     })
   };
 
-  // Execute immediate token verification on page-load.
-
-  angular.element(document).ready(function () {
-    console.log('page loaded')
-    $scope.validateInvite();
-  });
 });
